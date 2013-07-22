9586f5f6d91712042b311a67d42e94054c990870
Zeb McCorkle <zmccorle@creativevg.tk>
2013-07-22 14:45:22 -0400

    Created project

---
+++ src/main/java/tk/mypalsgaming/TARDIScraft/App.java
@@ -0,0 +1,13 @@
+package tk.mypalsgaming.TARDIScraft;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
